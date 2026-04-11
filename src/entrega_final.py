fec157289d17263d1e1abe77a796069ceb4cfd31
franoHub <francogimenez100@gmail.com>
2026-04-11 16:21:17 -0300

    test3

--- src/entrega_final.py
+++ src/entrega_final.py
@@ -14,6 +14,7 @@ import os
 
 
 #test_github
+#test
 #-------------------------------------------------FUNCIONES------------------------------------------------------
 #-----------------------------------------BEAUTIFUL SOUP & REQUESTS----------------------------------------------
 def clima_caba(): #Función para obtener el clima de CABA en tiempo real
